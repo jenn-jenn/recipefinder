c3b9ec7690a1126aa50a80852dde2cc6caca3f60
Jennifer <jennijenjen9@gmail.com>
2016-07-06 23:35:11 -0700

    Param search -> chocolate; added root route

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   def index
   	# check if request parameter search was passed in
   	# use the search term as the keyword if supplied, & use a default value of chocolate if not supplied
-  	@search = "chocolate"
-  	@results = Recipe.for(@search)
+  	@search_term = params[:search] || 'chocolate'
+  	@results = Recipe.for(@search_term)
   end
 end
