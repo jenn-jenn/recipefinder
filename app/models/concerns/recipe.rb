fe924511f37565833fd2b3c1c1b12ba51ca7c9a2
Jennifer <jennijenjen9@gmail.com>
2016-07-07 18:58:33 -0700

    added default_params, made tables, used image_tag, link_to helpers

--- app/models/concerns/recipe.rb
+++ app/models/concerns/recipe.rb
@@ -4,7 +4,8 @@ class Recipe
 	key_value = ENV['FOOD2FORK_KEY']
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api"
-	default_params key: ENV["FOOD2FORK_KEY"]
+	default_params key: ENV["FOOD2FORK_KEY"], fields: "title,image_url,social_rank,source_url", q: "search"
+	format :json
 
 	def self.for keyword
 		get("/search", query: {q: keyword})["recipes"]
