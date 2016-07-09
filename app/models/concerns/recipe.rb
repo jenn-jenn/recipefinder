6f239589010aa26fb00ef0365859d85bddcca0bd
Jennifer <jennijenjen9@gmail.com>
2016-07-09 16:41:37 -0700

    Finished! Need to run server first then rspec, due to server was not on

--- app/models/concerns/recipe.rb
+++ app/models/concerns/recipe.rb
@@ -1,14 +1,15 @@
+
 class Recipe
 	include HTTParty
 
 	key_value = ENV['FOOD2FORK_KEY']
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api"
-	default_params key: ENV["FOOD2FORK_KEY"], fields: "title,image_url,social_rank,source_url", q: "search"
+	default_params key: ENV["FOOD2FORK_KEY"], q: "search"
 	format :json
 
-	def self.for keyword
-		get("/search", query: {q: keyword})["recipes"]
+	def self.for term
+		get("/search", query: {q: term})["recipes"]
 	end
 
 end
